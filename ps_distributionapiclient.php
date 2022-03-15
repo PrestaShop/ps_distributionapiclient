85e6d125ba11ec85dd47ddd2aad892a3fba9bd4d
Thomas BACCELLI <tbaccelli@gmail.com>
2022-03-15 14:54:08 +0100

    First commit

---
+++ ps_distributionapiclient.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+use PrestaShop\Module\DistributionApiClient\DistributionApi;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+class Ps_Distributionapiclient extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'ps_distributionapiclient';
+        $this->displayName = $this->trans('Distribution API Client');
+        $this->description = $this->trans('Download and upgrade PrestaShop\'s native modules.');
+        $this->author = 'PrestaShop';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => _PS_VERSION_];
+        $this->tab = 'market_place';
+        parent::__construct();
+    }
+
+    public function install(): bool
+    {
+        return parent::install()
+            && $this->registerHook('actionListModules')
+            && $this->registerHook('actionBeforeInstallModule')
+            && $this->registerHook('actionBeforeUpgradeModule')
+        ;
+    }
+
+    /**
+     * @return array<array<string, string>>
+     */
+    public function hookActionListModules(): array
+    {
+        return $this->getDistributionApi()->getModuleList();
+    }
+
+    /**
+     * @param string[] $params
+     *
+     * @return void
+     */
+    public function hookActionBeforeInstallModule(array $params): void
+    {
+        if (!isset($params['moduleName'])) {
+            return;
+        }
+
+        $this->getDistributionApi()->downloadModule($params['moduleName']);
+    }
+
+    /**
+     * @param string[] $params
+     *
+     * @return void
+     */
+    public function hookActionBeforeUpgradeModule(array $params): void
+    {
+        if (!isset($params['moduleName'])) {
+            return;
+        }
+
+        $this->getDistributionApi()->downloadModule($params['moduleName']);
+    }
+
+    private function getDistributionApi(): DistributionApi
+    {
+        /** @var DistributionApi $distributionApi */
+        $distributionApi = $this->get('distributionapiclient.distribution_api');
+
+        return $distributionApi;
+    }
+}
---
+++ src/DistributionApi.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\DistributionApiClient;
+
+use PrestaShop\CircuitBreaker\Contract\CircuitBreakerInterface;
+use PrestaShop\PrestaShop\Core\Module\SourceHandler\SourceHandlerFactory;
+
+class DistributionApi
+{
+    public const ALLOWED_FAILURES = 2;
+    public const TIMEOUT_IN_SECONDS = 3;
+    public const THRESHOLD_SECONDS = 86400; // 24 hours
+    public const CACHE_LIFETIME_SECONDS = 86400; // 24 hours
+
+    private const API_ENDPOINT = 'http://localhost/prestashop-api/public';
+
+    /** @var CircuitBreakerInterface */
+    private $circruitBreaker;
+
+    /** @var SourceHandlerFactory */
+    private $sourceHandlerFactory;
+
+    /** @var string */
+    private $prestashopVersion;
+
+    /** @var string */
+    private $downloadDirectory;
+
+    public function __construct(
+        CircuitBreakerInterface $circruitBreaker,
+        SourceHandlerFactory $sourceHandlerFactory,
+        string $prestashopVersion,
+        string $downloadDirectory
+    ) {
+        $this->circruitBreaker = $circruitBreaker;
+        $this->sourceHandlerFactory = $sourceHandlerFactory;
+        $this->prestashopVersion = $prestashopVersion;
+        $this->downloadDirectory = trim($downloadDirectory, '/');
+    }
+
+    /**
+     * @return array<array<string, string>>
+     */
+    public function getModuleList(): array
+    {
+        $endpoint = self::API_ENDPOINT . '/modules/' . $this->prestashopVersion;
+        $response = $this->getResponse($endpoint);
+
+        $modules = [];
+
+        foreach ($response as $name => $module) {
+            $modules[] = [
+                'name' => $name,
+                'displayName' => $module['displayName'],
+                'description' => $module['description'],
+                'version' => $module['version'],
+                'version_available' => $module['version'],
+                'author' => $module['author'],
+                'download_url' => $module['download_url'],
+                'img' => $module['icon'],
+                'tab' => $module['tab'],
+            ];
+        }
+
+        return $modules;
+    }
+
+    public function downloadModule(string $moduleName): void
+    {
+        $modules = $this->getModuleList();
+        foreach ($modules as $module) {
+            if ($module['name'] === $moduleName) {
+                $this->doDownload($module);
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param array<string, string> $module
+     *
+     * @return void
+     */
+    private function doDownload(array $module): void
+    {
+        if (!isset($module['download_url'])) {
+            return;
+        }
+
+        $moduleZip = file_get_contents($module['download_url']);
+
+        $downloadPath = $this->getModuleDownloadDirectory($module['name']);
+        $this->createDownloadDirectoryIfNeeded($downloadPath);
+
+        file_put_contents($this->getModuleDownloadDirectory($module['name']), $moduleZip);
+
+        $handler = $this->sourceHandlerFactory->getHandler($this->getModuleDownloadDirectory($module['name']));
+        $handler->handle($this->getModuleDownloadDirectory($module['name']));
+    }
+
+    private function getModuleDownloadDirectory(string $moduleName): string
+    {
+        return $this->downloadDirectory . '/' . $moduleName . '.zip';
+    }
+
+    private function createDownloadDirectoryIfNeeded(string $downloadPath): void
+    {
+        if (!file_exists(dirname($downloadPath))) {
+            mkdir(dirname($downloadPath), 0777, true);
+        }
+    }
+
+    /**
+     * @param string $endpoint
+     *
+     * @return array<array<string, string>>
+     */
+    private function getResponse(string $endpoint): array
+    {
+        /** @var array<array<string, string>> $response */
+        $response = json_decode($this->circruitBreaker->call($endpoint), true) ?: [];
+
+        return $response;
+    }
+}
