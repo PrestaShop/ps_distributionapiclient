67d9f1fc66b135c1e6da1e333fcba0c2b295e90c
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-09-01 12:52:32 +0200

    Bumpversion 1.0.2

--- ps_distributionapiclient.php
+++ ps_distributionapiclient.php
@@ -38,7 +38,7 @@ class Ps_Distributionapiclient extends Module
         $this->displayName = $this->trans('Distribution API Client', [], 'Modules.Distributionapiclient.Admin');
         $this->description = $this->trans('Download and upgrade PrestaShop\'s native modules.', [], 'Modules.Distributionapiclient.Admin');
         $this->author = 'PrestaShop';
-        $this->version = '1.0.1';
+        $this->version = '1.0.2';
         $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => _PS_VERSION_];
         $this->tab = 'market_place';
         parent::__construct();
