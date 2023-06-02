5cc2d8f4ef36d6582ecdf2d95f3a101cd2072773
Krystian Podemski <podemski.krystian@gmail.com>
2023-06-02 14:10:25 +0200

    asd

--- tests/phpstan.sh
+++ tests/phpstan.sh
@@ -27,5 +27,5 @@ docker run --rm --volumes-from temp-ps \
        -v $PWD:/var/www/html/modules/ps_distributionapiclient \
        -e _PS_ROOT_DIR_=/var/www/html \
        --workdir=/var/www/html/modules/ps_distributionapiclient ghcr.io/phpstan/phpstan:1.4.10 \
-       analyse --vvv --memory-limit=-1 \
+       analyse --debug --memory-limit=-1 \
        --configuration=/var/www/html/modules/ps_distributionapiclient/tests/phpstan/phpstan-${PS_VERSION}.neon
